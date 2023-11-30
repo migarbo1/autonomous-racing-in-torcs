707668b32126c79c77732c24bec4669e2cdea9b8
migarbo1 <miguel.garciabohigues@hotmail.com>
2023-11-30 17:58:33 +0100

    models saved at the end of the execution

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import gymnasium as gym
 from ppo.ppo import PPO
 from torcs_python_client.torcs_env import TorcsEnv
+import torch
 
 if __name__ == '__main__':
     env = TorcsEnv()
     model = PPO(env)
     model.learn(100000)
+    torch.save(model.actor.state_dict(), './ppo_actor.pth')
+    torch.save(model.critic.state_dict(), './ppo_critic.pth')
+
