f005ed5d73f30e37d9743968b2acdf7f5d13cbca
migarbo1 <miguel.garciabohigues@hotmail.com>
2023-12-14 17:05:37 +0100

    exception handler added to main

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ from torcs_python_client.torcs_env import TorcsEnv
 import torch
 import os
 
-if __name__ == '__main__':
-    env = TorcsEnv()
-    model = PPO(env)
-    model.learn(500000)
-    env.save_laptimes()
+def training_finished_procedure(env: TorcsEnv, model: PPO):
+    env.training_data['total_training_timesteps'] += model.current_timesteps
+    env.save_training_data()
     torch.save(model.actor.state_dict(), './ppo_actor.pth')
     torch.save(model.critic.state_dict(), './ppo_critic.pth')
     os.system(f'pkill torcs')
+
+if __name__ == '__main__':
+    env = TorcsEnv()
+    #TODO: make console parameter
+    timesteps = 50000
+    model = PPO(env)
+    try:
+        model.learn(timesteps)
+    except:
+        training_finished_procedure(env, model)
+    training_finished_procedure(env, model)
+
+    
 
