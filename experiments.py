69e2f8a7ac4612a5c32eae4f5c1e5020de592374
migarbo1 <miguel.garciabohigues@hotmail.com>
2024-02-12 19:36:31 +0100

    interval trust added to results

--- experiments.py
+++ experiments.py
@@ -4,6 +4,8 @@ from ppo.ppo import PPO
 from pathlib import Path
 import numpy as np
 import torch
+import time
+import math
 import os
 
 def change_track(track, prev_track):
@@ -30,7 +32,8 @@ def write_results(results):
         file.write(total_text)
 
 if __name__ == '__main__':
-    snakeoil.set_textmode(False)
+    torch.set_default_device('cuda')
+    # snakeoil.set_textmode(False)
     snakeoil.set_tracks(track_list=['quickrace'])
     tracks = ['g-track-1', 'forza', 'g-track-2', 'g-track-3', 'ole-road-1', 'ruudskogen', 'street-1', 'wheel-1', 'wheel-2', 'aalborg', 'alpine-1', 'alpine-2', 'e-track-1', 'e-track-2', 'e-track-4', 'e-track-6', 'eroad', 'e-track-3']
     results= {}
@@ -41,19 +44,24 @@ if __name__ == '__main__':
         rollout_rewards = []
         avg_speeds = []
         max_speeds = []
-        for i in range(5):
-            env = TorcsEnv()
-            model = PPO(env, test=True)
-            model.eval_max_timesteps = 500000
-            model.launch_eval()
-            env.kill_torcs()
-            rollout_distances.append(model.env.training_data['eval_results'][-1]['dist_raced'])
-            
-            r = compute_reward(model.env.training_data['eval_results'][-1]['observation_rollout'])
-            rollout_rewards.append(r)
-            
-            avg_speeds.append(model.env.training_data['eval_results'][-1]['avg_speed'])
-            max_speeds.append(model.env.training_data['eval_results'][-1]['max_speed'])
+        for i in range(10):
+            try:
+                env = TorcsEnv()
+                model = PPO(env, test=True)
+                model.eval_max_timesteps = 50000
+                model.launch_eval(only_practice=False)
+                env.kill_torcs()
+                rollout_distances.append(model.env.training_data['eval_results'][-1]['dist_raced'])
+                
+                r = compute_reward(model.env.training_data['eval_results'][-1]['observation_rollout'])
+                rollout_rewards.append(r)
+                
+                avg_speeds.append(model.env.training_data['eval_results'][-1]['avg_speed'])
+                max_speeds.append(model.env.training_data['eval_results'][-1]['max_speed'])
+            except Exception as e:
+                print(e)
         prev_track = track
-        results[track] = '{' + f'max_rollout_dist: {np.max(rollout_distances):4f}, avg_rollout_dist: {np.mean(rollout_distances):4f}, min_rollout_dist: {np.min(rollout_distances):4f}, max_score: {np.max(rollout_rewards):4f}, avg_score: {np.mean(rollout_rewards):4f}, avg_max_speed: {np.mean(max_speeds):4f}, avg_avg_speed: {np.mean(avg_speeds):4f}' + '}'
+        rollout_conf_int = 1.95 * (np.std(rollout_distances)/math.sqrt(len(rollout_distances)))
+        results[track] = '{' + f'max_rollout_dist: {np.max(rollout_distances):4f}, avg_rollout_dist: {np.mean(rollout_distances):4f}, min_rollout_dist: {np.min(rollout_distances):4f}, rollout_conf_int: {rollout_conf_int:4f}, max_score: {np.max(rollout_rewards):4f}, avg_score: {np.mean(rollout_rewards):4f}, avg_max_speed: {np.mean(max_speeds):4f}, avg_avg_speed: {np.mean(avg_speeds):4f}' + '}'
     write_results(results)
+    change_track('brondehach', tracks[-1])
--- ppo/ppo.py
+++ ppo/ppo.py
@@ -98,7 +98,7 @@ class PPO:
         return future_rewards_batch
 
 
-    def launch_eval(self):
+    def launch_eval(self, only_practice=True):
         eval_results = {}
 
         i = 0
@@ -111,7 +111,7 @@ class PPO:
         last_lap_time = 0 
 
         done = False
-        state, _ = self.env.reset(eval=True)
+        state, _ = self.env.reset(eval=only_practice)
         while not done and i < self.eval_max_timesteps:
             action, log_prob = self.get_action(state)
             state, reward, done, _ ,_ = self.env.step(action)
