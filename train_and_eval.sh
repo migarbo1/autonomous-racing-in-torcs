18bc5186859bc9e5b5a72676ecec18cbf286c9e4
migarbo1 <miguel.garciabohigues@hotmail.com>
2024-07-30 19:33:00 +0200

    trainig procedure

--- train_and_eval.sh
+++ train_and_eval.sh
@@ -1,29 +1,49 @@
-# ##############################################################
+##############################################################################################################################
 
-# name="ppo_HuC"
+name="M1_var_01"
 
-# python main.py -n "./tfm/$name" -j -f 5 --focus
+python main.py -n "./results/AAAI/$name" -j --min_variance 0.01
 
-# python experiments.py -n "./tfm/$name" -j --output_file "./tfm/${name}.txt" -f 5 --focus
+python experiments.py -n "./results/AAAI/$name" -j --output_file "./results/AAAI/${name}.txt" --min_variance 0.01
 
-# python experiments.py -n "./tfm/${name}_last" -j --output_file "./tfm/${name}_last.txt" -f 5 --focus
+python experiments.py -n "./results/AAAI/${name}_last" -j --output_file "./results/AAAI/${name}_last.txt" --min_variance 0.01
 
-# ##############################################################
+##############################################################################################################################
 
-# name="ppo_HuBe"
+name="BotBeh_var_01"
 
-# python main.py -n "./tfm/$name" -j -h --hf "./human_data/formatted_fs1.pickle"
+python main.py -n "./results/AAAI/$name" -j --min_variance 0.01 -h --hf './human_data/inferno_formatted.pickle'
 
-# python experiments.py -n "./tfm/$name" -j --output_file "./tfm/${name}.txt" 
+python experiments.py -n "./results/AAAI/$name" -j --output_file "./results/AAAI/${name}.txt" --min_variance 0.01
 
-# python experiments.py -n "./tfm/${name}_last" -j --output_file "./tfm/${name}_last.txt" 
+python experiments.py -n "./results/AAAI/${name}_last" -j --output_file "./results/AAAI/${name}_last.txt" --min_variance 0.01
 
-# ##############################################################
+##############################################################################################################################
 
-name="ppo_HuBeC"
+name="HuBeh_var_01"
 
-python main.py -n "./tfm/$name" -j -f 5 -h --focus --hf "./human_data/formatted_fs5.pickle"
+python main.py -n "./results/AAAI/$name" -j --min_variance 0.01 -h --hf './human_data/human_formatted.pickle'
 
-python experiments.py -n "./tfm/$name" -j --output_file "./tfm/${name}.txt" -f 5 --focus
+python experiments.py -n "./results/AAAI/$name" -j --output_file "./results/AAAI/${name}.txt" --min_variance 0.01
 
-python experiments.py -n "./tfm/${name}_last" -j --output_file "./tfm/${name}_last.txt" -f 5 --focus
+python experiments.py -n "./results/AAAI/${name}_last" -j --output_file "./results/AAAI/${name}_last.txt" --min_variance 0.01
+
+##############################################################################################################################
+
+name="BotBeh"
+
+python main.py -n "./results/AAAI/$name" -j -h --hf './human_data/inferno_formatted.pickle'
+
+python experiments.py -n "./results/AAAI/$name" -j --output_file "./results/AAAI/${name}.txt"
+
+python experiments.py -n "./results/AAAI/${name}_last" -j --output_file "./results/AAAI/${name}_last.txt"
+
+##############################################################################################################################
+
+name="HuBeh"
+
+python main.py -n "./results/AAAI/$name" -j -h --hf './human_data/human_formatted.pickle'
+
+python experiments.py -n "./results/AAAI/$name" -j --output_file "./results/AAAI/${name}.txt"
+
+python experiments.py -n "./results/AAAI/${name}_last" -j --output_file "./results/AAAI/${name}_last.txt" 
