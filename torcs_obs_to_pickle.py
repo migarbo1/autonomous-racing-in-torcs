18dfdce5e4295e9c3177699079fe65132f50ec57
migarbo1 <miguel.garciabohigues@hotmail.com>
2024-08-02 12:03:26 +0200

    more data recorded by humans

--- torcs_obs_to_pickle.py
+++ torcs_obs_to_pickle.py
@@ -4,7 +4,10 @@ import click
 import sys
 import os
 
-def compute_spbased_reward(speed_x, speed_y, angle, prev_speed, prev_angle):
+def compute_spbased_reward(speed_x, speed_y, angle, prev_speed, prev_angle, track_pos):
+
+    if abs(track_pos) > 1.0:
+        return -10000
 
     speed_dif = abs(speed_x - prev_speed)
     
@@ -16,6 +19,9 @@ def compute_spbased_reward(speed_x, speed_y, angle, prev_speed, prev_angle):
     return speed_reward - angle_variation
 
 def compute_distbased_reward(angle, track_pos, dist_raced, prev_dist_raced):
+
+    if abs(track_pos) > 1.0:
+        return -10000
 
     return (dist_raced-prev_dist_raced) * np.cos(angle) * (1 - abs(track_pos))
 
@@ -93,13 +99,13 @@ def main(num_frames, reward_type, join_accel_brake, prefix):
             action_list.append(action)
 
             if reward_type == 'speed':
-                reward = compute_spbased_reward(speed_x/300., speed_y/300., angle, prev_speed, prev_angle)
+                reward = compute_spbased_reward(speed_x/300., speed_y/300., angle, prev_speed, prev_angle, track_pos)
             else:
                 reward = compute_distbased_reward(angle, track_pos, dist_raced, prev_dist_raced)
             reward_list.append(reward)
             
             prev_dist_raced = dist_raced
-            done_list.append(False)
+            done_list.append(abs(track_pos)>1.0)
             prev_angle = angle if not use_fs else fs[0][0]
             prev_speed = speed_x/300. if not use_fs else fs[0][1]
 
